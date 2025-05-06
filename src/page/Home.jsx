1460112af8332c1092e705d1c5e4e7afc4f62942
Yug Patel <yugpatelvipulbhai@gmail.com>
2025-05-06 15:12:38 +0530

    Cr

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -581,7 +581,7 @@ const Home = () => {
                       >
                         {Chatloader ? (
                           <Box display="flex" justifyContent="center" alignItems="center" flexGrow={1}>
-                            <CircularProgress size={50} thickness={3} sx={{ color: 'silver' }} />
+                            <CircularProgress size={50} thickness={3} sx={{ color: 'gray' }} />
 
                           </Box>
                         ) : (
