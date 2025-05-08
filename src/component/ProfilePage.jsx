e70453987712d09b7e128a0f6e4c1733ed72d12a
Yug Patel <yugpatelvipulbhai@gmail.com>
2025-05-08 12:11:36 +0530

    suggested scroll

--- src/component/ProfilePage.jsx
+++ src/component/ProfilePage.jsx
@@ -673,6 +673,7 @@ const ProfilePage = () => {
                             border: '1px solid rgba(255, 255, 255, 0.1)',
                             boxShadow: '0 8px 32px rgba(0, 0, 0, 0.3)',
                             overflowX:'auto',
+                            height:'200px'
                         }}
                     >
                         <Box
