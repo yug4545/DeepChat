046bfc7900e3bfa6d3ce8d598aa3aeacbfb6fb59
Yug Patel <yugpatelvipulbhai@gmail.com>
2025-05-09 14:57:13 +0530

    last message

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -505,11 +505,7 @@ const Home = () => {
                     </Avatar>
                     <Box>
                       <Typography fontWeight={600} color="#fff">{user?.username}</Typography>
-                      <Typography fontSize={12} color="#888">{
-                        Array.isArray(receivedMessages) && receivedMessages.length > 0
-                          ? receivedMessages[receivedMessages.length - 1]
-                          : "No messages yet"
-                      }</Typography>
+                      <Typography fontSize={12} color="#888">Following</Typography>
                     </Box>
                   </Box>
 
