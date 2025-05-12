cc61d51cb2e1dd1facc435387f591e2289dd188f
Yug Patel <yugpatelvipulbhai@gmail.com>
2025-05-12 15:42:37 +0530

    snakber

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -268,9 +268,6 @@ const Home = () => {
 
   const action = (
     <React.Fragment>
-      <Button color="secondary" size="small" onClick={handleClose}>
-        UNDO
-      </Button>
       <IconButton
         size="small"
         aria-label="close"
